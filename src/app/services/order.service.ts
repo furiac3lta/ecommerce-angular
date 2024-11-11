412ea669da9270c456345a0f216c72d1130f55d9
furiac3lta <colummarcelo@gmail.com>
2024-11-11 20:58:40 -0300

    add storage for change status order

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+/* import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Order } from '../common/order';
 import { Observable } from 'rxjs';
@@ -24,5 +24,38 @@ export class OrderService {
   }
   getOrderById(orderId: number): Observable<Order> {
     return this.httpClient.get<Order>(`${this.apiUrl}/${orderId}`, {headers: this.headerService.headers});
+  }
+}
+ */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Order } from '../common/order';
+import { Observable } from 'rxjs';
+import { HeaderService } from './header.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OrderService {
+  private apiUrl: string = 'https://ecommerce-back-0cc9b90e39e5.herokuapp.com/api/v1/orders';
+  private updateEndpoint: string = '/update/state/order';
+
+  constructor(private httpClient: HttpClient, private headerService: HeaderService) {}
+
+  createOrder(order: Order): Observable<Order> {
+    return this.httpClient.post<Order>(this.apiUrl, order, { headers: this.headerService.headers });
+  }
+
+  updateOrderStatus(orderId: number, status: string): Observable<void> {
+    const updateUrl = `${this.apiUrl}${this.updateEndpoint}?id=${orderId}&state=${status}`;
+    return this.httpClient.post<void>(updateUrl, {}, { headers: this.headerService.headers });
+  }
+
+  getOrderByUser(userId: number): Observable<Order[]> {
+    return this.httpClient.get<Order[]>(`${this.apiUrl}/by-user/${userId}`, { headers: this.headerService.headers });
+  }
+
+  getOrderById(orderId: number): Observable<Order> {
+    return this.httpClient.get<Order>(`${this.apiUrl}/${orderId}`, { headers: this.headerService.headers });
   }
 }
