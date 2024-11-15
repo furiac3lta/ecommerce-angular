46e94e39608cd0e992aa83b044313f730caea3b2
furiac3lta <colummarcelo@gmail.com>
2024-11-15 17:55:26 -0300

    change enviroment key

--- src/enviroments/enviroment.prod.ts
+++ src/enviroments/enviroment.prod.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
     production: true,
-    mercadoPagoPublicKey: 'APP_USR-f089d4da-a397-462a-a47c-f39ac4d89311'
+    mercadoPagoPublicKey: 'APP_USR-b2af9317-8e53-45eb-a195-1eb41c633f94'
   };
   
--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: true,
-  mercadoPagoPublicKey: 'APP_USR-f089d4da-a397-462a-a47c-f39ac4d89311',
+  mercadoPagoPublicKey: 'APP_USR-b2af9317-8e53-45eb-a195-1eb41c633f94',
 };
