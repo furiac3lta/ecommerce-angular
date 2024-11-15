00422fc081fa1dec003d5934035dca37e4c7f143
furiac3lta <colummarcelo@gmail.com>
2024-11-15 09:10:01 -0300

    change title hero

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,7 +1,9 @@
-import { Component, AfterViewInit, ViewChild, ElementRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, ViewChild, ElementRef, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SwiperCustomElementService } from 'src/app/services/swiper-custom-element.service';
 import {MatButtonModule} from '@angular/material/button';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-hero',
@@ -11,31 +13,37 @@ import {MatButtonModule} from '@angular/material/button';
   styleUrls: ['./hero.component.css'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
+
+
 export class HeroComponent {
   @ViewChild('swiper',{static: false}) swiperEl!: ElementRef;
   slides = [
     {
-      title: 'Top 5 Benefits of Using a Daily Moisturizer',
-      subtitle: 'perfect match',
-      description: 'Choose the right moisturizer for healthy, glowing skin with our guide to tailored formulas and ingredients for your skin',
+      title: 'El Acondicionador Perfecto para tu Cabello',
+      subtitle: 'combinación ideal',
+      description: 'Encuentra el acondicionador ideal para un cabello saludable y brillante con nuestra guía de fórmulas e ingredientes específicos para tu tipo de cabello.',
       image: 'https://nov-costica.myshopify.com/cdn/shop/files/s-6-3.jpg?v=1728016535&width=1500',
       button: 'Comprar ahora'
     },
     {
-      title: 'Top 5 Benefits of Using a Daily Moisturizer',
-      subtitle: 'perfect match',
-      description: 'Choose the right moisturizer for healthy, glowing skin with our guide to tailored formulas and ingredients for your skin',
+      title: 'Elige el Tratamiento Ideal para un Cabello Radiante',
+      subtitle: 'combinación ideal',
+      description: 'Escoge el tratamiento adecuado para un cabello radiante y bien cuidado siguiendo nuestra guía con las mejores fórmulas e ingredientes según tus necesidades.',
       image: 'https://nov-costica.myshopify.com/cdn/shop/files/s-6-2.jpg?v=1728016535&width=1500',
       button: 'Comprar ahora'
     },{
-      title: 'Top 5 Benefits of Using a Daily Moisturizer',
-      subtitle: 'perfect match',
-      description: 'Choose the right moisturizer for healthy, glowing skin with our guide to tailored formulas and ingredients for your skin',
+      title: 'Cómo Hidratar tu Cabello para un Brillo Saludable',
+      subtitle: 'combinación ideal',
+      description: 'Descubre cómo elegir el mejor producto hidratante para mantener tu cabello sano y luminoso con nuestra guía adaptada a cada tipo de cabello.',
       image: 'https://nov-costica.myshopify.com/cdn/shop/files/s-6-1.jpg?v=1728016536&width=2000',
       button: 'Comprar ahora'
     }
   ]
-  constructor(private swiperCustomElementService: SwiperCustomElementService) {}
+  constructor(private swiperCustomElementService: SwiperCustomElementService,private router: Router) {}
+
+  goToDetail() {
+    this.router.navigate(['/product']);
+  }
 
   ngAfterViewInit() {
     const swiperInstance = this.swiperEl.nativeElement.swiper;
