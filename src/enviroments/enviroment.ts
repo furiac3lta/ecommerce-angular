5cecffaf619ff4f8e98824dbc01b73e7bfad89de
furiac3lta <colummarcelo@gmail.com>
2026-02-11 13:30:00 -0300

    last changes 6

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,11 +1,11 @@
-// src/environments/environment.ts
 export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost:8085',
-  transferAlias: 'LIONS.BRAND',
-  transferCbu: '0000000000000000000000',
-  transferBank: 'Banco',
-  transferHolder: 'Lions Brand',
-  whatsappNumber: '+5492966547041',
-  mercadoPagoPublicKey: 'APP_USR-b2af9317-8e53-45eb-a195-1eb41c633f94',
-};
+    production: true,
+    apiBaseUrl: 'https://ecommerce-backend-production-3fca.up.railway.app',
+    transferAlias: 'LIONS.BRAND',
+    transferCbu: '0000000000000000000000',
+    transferBank: 'Banco',
+    transferHolder: 'Lions Brand',
+    whatsappNumber: '5490000000000',
+    mercadoPagoPublicKey: 'APP_USR-b2af9317-8e53-45eb-a195-1eb41c633f94'
+  };
+  
