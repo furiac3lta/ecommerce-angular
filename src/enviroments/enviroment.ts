43610533e60eb2a77177ae620f5f24fe7424767c
furiac3lta <colummarcelo@gmail.com>
2026-03-07 10:24:19 -0300

    add some info

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -5,7 +5,7 @@ export const environment = {
     transferCbu: '0000000000000000000000',
     transferBank: 'Banco',
     transferHolder: 'Lions Brand',
-    whatsappNumber: '5490000000000',
+    whatsappNumber: '+5491128688430',
     mercadoPagoPublicKey: 'APP_USR-b2af9317-8e53-45eb-a195-1eb41c633f94'
   };
   
